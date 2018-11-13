d154b9c00e841b3ac777fd2633345cea90964400
Hristo Nikolov <h.n.nikolov@gmail.com>
2018-11-13 17:49:15 +0100

    Minor position change of components

--- ws_lcd/layout_222.py
+++ ws_lcd/layout_222.py
@@ -8,7 +8,7 @@ class Layout_222(Layout):
         super(Layout_222, self).__init__(color = "black")
         
         self.ch1     = 18 # component height 1
-        self.ch2     = 25 # component height 2
+        self.ch2     = 26 # component height 2
         self.sh1     =  2 # separator height 1
         self.bar     = 25
 
@@ -19,7 +19,7 @@ class Layout_222(Layout):
         self.sep_3_y = self.row_2_y + self.ch2
         self.row_3_y = self.sep_3_y + self.sh1
         self.sep_4_y = self.row_3_y + self.ch2
-        self.row_4_y = self.sep_4_y + self.sh1 + 2
+        self.row_4_y = self.sep_4_y + self.sh1
 
         # Build the layout
         self.cdate   = Component(72, self.ch1, font_size=14, bg_color=0, align=1)
@@ -35,11 +35,11 @@ class Layout_222(Layout):
         self.wi.set_position(4, self.row_1_y)
 #        self.wi.draw_borders()
 
-        self.wv   = Component(64, self.ch2, font_size=18)
-        self.wv.set_position(28, self.row_1_y)
+        self.wv   = Component(68, self.ch2, font_size=18)
+        self.wv.set_position(30, self.row_1_y)
 
-        self.wu   = Component(32, self.ch2, font_size=16)
-        self.wu.set_position(92, self.row_1_y)
+        self.wu   = Component(self.ch2, self.ch2, font_size=16)
+        self.wu.set_position(98, self.row_1_y)
         self.wu.set_text("Lit", 0, align=0)
 
         self.gi   = Component(self.ch2, self.ch2, font_size=20, image='gas_32x32.png')
@@ -47,21 +47,21 @@ class Layout_222(Layout):
 #        self.gi.set_text("1h", align=1)
         self.gi.set_position(4, self.row_2_y)
 
-        self.gv   = Component(64, self.ch2, font_size=18, format_string="{0:.2f}")
-        self.gv.set_position(28, self.row_2_y)
+        self.gv   = Component(68, self.ch2, font_size=18, format_string="{0:.2f}")
+        self.gv.set_position(30, self.row_2_y)
 
-        self.gu   = Component(32, self.ch2, font_size=16)
-        self.gu.set_position(92, self.row_2_y)
+        self.gu   = Component(self.ch2, self.ch2, font_size=16)
+        self.gu.set_position(98, self.row_2_y)
         self.gu.set_text("m" + u'\u00B3', 0, align=0)
 
         self.ei   = Component(self.ch2, self.ch2, font_size=20, image='plug1.png')
         self.ei.set_position(4, self.row_3_y)
 
-        self.ev   = Component(64, self.ch2, font_size=18, format_string="{0:.3f}")
-        self.ev.set_position(28, self.row_3_y)
+        self.ev   = Component(68, self.ch2, font_size=18, format_string="{0:.3f}")
+        self.ev.set_position(30, self.row_3_y)
 
-        self.eu   = Component(32, self.ch2, font_size=16)
-        self.eu.set_position(92, self.row_3_y)
+        self.eu   = Component(self.ch2, self.ch2, font_size=16)
+        self.eu.set_position(98, self.row_3_y)
         self.eu.set_text("kW", 0, align=0)
 
         self.egraph = BarGraph(128, self.bar, bg_color=0)
@@ -130,14 +130,14 @@ if __name__ == '__main__':
         
     idx = 0
     for j in range(4):   
-     for i in range(6):
-        L2.wv.add(1)
-        L2.gv.add(0.01)
-        L2.set_date_time()
-        L2.egraph.set_bar(idx, float(2.11*(i+1)))
-        print float(2.11*(i+1))
-        lcd.update(L2)
-        idx += 1
-        raw_input()
+        for i in range(6):
+            L2.wv.add(1)
+            L2.gv.add(0.01)
+            L2.set_date_time()
+            L2.egraph.set_bar(idx, float(2.11*(i+1)))
+#            print float(2.11*(i+1))
+            lcd.update(L2)
+            idx += 1
+            raw_input()
         
     lcd.close()
