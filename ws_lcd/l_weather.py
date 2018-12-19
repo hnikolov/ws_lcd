b29bda963f073b130228196b7b7b4d3533567d50
Hristo Nikolov <h.n.nikolov@gmail.com>
2018-12-19 12:18:16 +0100

    Some cleaning

--- ws_lcd/l_weather.py
+++ ws_lcd/l_weather.py
@@ -3,11 +3,10 @@ import time
 from layout import Layout
 from component import *
 
-class Layout_EUR(Layout):
+class LWeather(Layout):
     def __init__(self):
-        super(Layout_EUR, self).__init__(color = "black")
+        super(LWeather, self).__init__(color = "black")
 
-        self.ch1     = 18 # component height 1
         self.ch2     = 20 # component height 2
         self.sh1     =  2 # separator height 1
 
@@ -20,11 +19,12 @@ class Layout_EUR(Layout):
         self.sep_4_y = self.row_3_y + self.ch2
         self.row_4_y = self.sep_4_y + self.sh1
 
+        # N/A: u'\uf07b'
         
         # Build the layout
         self.mi   = Component(self.ch2, self.ch2, font='weathericons-regular-webfont.ttf', font_size=14, bg_color=0, align=1)
         self.mi.set_position(2, 0)
-        self.mi.set_text(u'\uF0E2', y=-1) # Moon phase
+        self.mi.set_text(u'\uF0E2', y=-1) # Moon phase, TODO use the 28 icons
         
         self.cdate = Component(52, self.ch2, font_size=14, bg_color=0, align=1)
         self.cdate.set_position(20, 0)
@@ -34,112 +34,81 @@ class Layout_EUR(Layout):
         self.ctime.set_position(72, 0)
         self.ctime.set(time.strftime('%H:%M'))
         # ----------------
-        self.ewi   = Component(48, 2*self.ch2, font_size=24, align=1)
-        self.ewi.set_position(0, self.row_1_y)
-        self.ewi.set_text("-23" + u'\N{DEGREE SIGN}')
+        self.tv   = Component(48, 2*self.ch2, font_size=24, format_string = "{0:.0f}" + u'\N{DEGREE SIGN}', align=1)
+        self.tv.set_position(0, self.row_1_y)
+        self.tv.set(-23.45)
 
-        self.ewu   = Component(48, 2*self.ch2, font='weathericons-regular-webfont.ttf', font_size=26, bg_color=255, align=1)
-        self.ewu.set_position(44, self.row_1_y)
-        self.ewu.set_text(u'\uF002') # Weather icon
+        self.ti   = Component(48, 2*self.ch2, font='weathericons-regular-webfont.ttf', font_size=26, align=1, bg_color=255)
+        self.ti.set_position(44, self.row_1_y)
+        self.ti.set_text(u'\uF002') # Weather icon
 
-        self.ewi0   = Component(34, self.ch2, font_size=14, align=0)
-        self.ewi0.set_position(94, self.row_1_y)
-        self.ewi0.set_text("-18" + u'\N{DEGREE SIGN}')
+        self.tmax   = Component(34, self.ch2, font_size=14, format_string = "{0:.0f}" + u'\N{DEGREE SIGN}', align=0)
+        self.tmax.set_position(94, self.row_1_y)
+        self.tmax.set(-18.87)
         
-        self.ewi1   = Component(34, self.ch2, font_size=14, align=0)
-        self.ewi1.set_position(94, self.row_1_y + self.ch2)
-        self.ewi1.set_text("-27" + u'\N{DEGREE SIGN}')
+        self.tmin   = Component(34, self.ch2, font_size=14, format_string = "{0:.0f}" + u'\N{DEGREE SIGN}', align=0)
+        self.tmin.set_position(94, self.row_1_y + self.ch2)
+        self.tmin.set(-27.64)
         # -----------------------------        
-        self.eti   = Component(92, self.ch2, font_size=13, font='Roboto-Condensed.ttf', align=1)
-        self.eti.set_position(0, self.row_2_y)
-        self.eti.set_text("Valkenburg, ZH")
+        self.ln   = Component(92, self.ch2, font_size=13, font='Roboto-Condensed.ttf', align=1)
+        self.ln.set_position(0, self.row_2_y)
+        self.ln.set_text("Valkenburg, ZH")
 
-        self.eti0   = Component(34, self.ch2, font_size=22, font='weathericons-regular-webfont.ttf', align=1)
-        self.eti0.set_position(94, self.row_2_y)
-        self.eti0.set_text(u'\uF058') # Arrow up: f058, down: f044
-
+        self.td   = Component(34, self.ch2, font_size=22, font='weathericons-regular-webfont.ttf', align=1) # temperature direction
+        self.td.set_position(94, self.row_2_y)
+        self.td.set_text(u'\uF058') # Arrow up: f058, down: f044
         # -------------------------------------        
-#        self.egi   = Component(self.ch2, self.ch2, font='weathericons-regular-webfont.ttf', font_size=20, bg_color=0)
-        self.egi   = Component(self.ch2, self.ch2, font='weathericons-regular-webfont.ttf', font_size=16, bg_color=0)
-        self.egi.set_position(0, self.row_3_y)
-#        self.egi.set_text(u'\uF078', align=1) # Humidity: F07A
-        self.egi.set_text(u'\uF07A', align=1) # Humidity
+        self.hi   = Component(self.ch2, self.ch2, font='weathericons-regular-webfont.ttf', font_size=16, bg_color=0)
+        self.hi.set_position(0, self.row_3_y)
+        self.hi.set_text(u'\uF07A', align=1) # Humidity, Water drop: u'\uF078'
 
-        self.egv   = Component(44, self.ch2, font_size=18, font='Roboto-Condensed.ttf', format_string = "{}%", align=0, bg_color=0)
-        self.egv.set_position(20, self.row_3_y)
-        self.egv.set(67)
+        self.hv   = Component(44, self.ch2, font_size=18, font='Roboto-Condensed.ttf', format_string = "{0:.0f}%", align=0, bg_color=0)
+        self.hv.set_position(20, self.row_3_y)
+        self.hv.set(67)
 
-        self.eri   = Component(24, self.ch2, font='weathericons-regular-webfont.ttf', font_size=16, bg_color=0)
-        self.eri.set_position(66, self.row_3_y)
-        self.eri.set_text(u'\uF084', align=1) # Umbrella
+        self.ri   = Component(24, self.ch2, font='weathericons-regular-webfont.ttf', font_size=16, bg_color=0)
+        self.ri.set_position(66, self.row_3_y)
+        self.ri.set_text(u'\uF084', align=1) # Umbrella
 
-        self.egu   = Component(38, self.ch2, font_size=18, font='Roboto-Condensed.ttf', bg_color=0)
-        self.egu.set_position(90, self.row_3_y)
-        self.egu.set_text("26%", align=0)
+        self.rv   = Component(38, self.ch2, font_size=18, font='Roboto-Condensed.ttf', format_string = "{0:.0f}%", align=0, bg_color=0)
+        self.rv.set_position(90, self.row_3_y)
+        self.rv.set(26.32)
         
-        self.eei   = Component(self.ch2, self.ch2, font_size=18, font='weathericons-regular-webfont.ttf', bg_color=0)
-        self.eei.set_position(0, self.row_4_y)
-        self.eei.set_text(u'\uF079', x=0, align=1) # Barometer
+        self.pi   = Component(self.ch2, self.ch2, font_size=18, font='weathericons-regular-webfont.ttf', bg_color=0)
+        self.pi.set_position(0, self.row_4_y)
+        self.pi.set_text(u'\uF079', x=0, align=1) # Barometer
 
-        self.eev   = Component(64, self.ch2, font_size=18, format_string="{0:.1f}", align=0, bg_color=0)
-        self.eev.set_position(20, self.row_4_y)
-        self.eev.set(1023.52)
+        self.pv   = Component(64, self.ch2, font_size=18, format_string="{0:.1f}", align=0, bg_color=0)
+        self.pv.set_position(20, self.row_4_y)
+        self.pv.set(1023.52)
 
-        self.eev0   = Component(34, self.ch2, font='weathericons-regular-webfont.ttf', font_size=20, bg_color=0, align=0)
-        self.eev0.set_position(94, self.row_4_y)
-        self.eev0.set_text(u'\uF0B7') # Wind
-        
-#        self.eeu   = Component(32, self.ch2, font_size=16)
-#        self.eeu.set_position(92, self.row_3_y)
-#        self.eeu.set_text(u'\u20AC', x=0, align=0) # Euro
-
-#        self.eti   = Component(128, 14, font_size=11, font='Roboto-Condensed.ttf', align=1)
-#        self.eti.set_position(0, self.row_4_y+8)
-#        self.eti.set_text("Overcast throughout the day.")
-
-#        self.etv   = Component(64, self.ch2, font_size=18, format_string="{0:.2f}")
-#        self.etv.set_position(28, self.row_4_y)
-
-#        self.etu   = Component(32, self.ch2, font_size=16)
-#        self.etu.set_position(92, self.row_4_y)
-#        self.etu.set_text(u'\u20AC', x=0, align=0) # Euro
+        self.wi   = Component(34, self.ch2, font='weathericons-regular-webfont.ttf', font_size=20, bg_color=0, align=0)
+        self.wi.set_position(94, self.row_4_y)
+        self.wi.set_text(u'\uF0B7') # Wind
 
         # Add components to the layout
         self.add([self.mi, self.cdate, self.ctime])
-#        self.add([self.ewi, self.ewv, self.ewu])
-        self.add([self.ewi,            self.ewu, self.ewi0, self.ewi1])
-#        self.add([self.egv, self.egu])
-        self.add([self.egi, self.egv, self.eri, self.egu])
-        self.add([self.eei, self.eev, self.eev0])
-#        self.add([self.eei, self.eev, self.eeu])
-        self.add([self.eti, self.eti0])
-#        self.add([self.eti, self.etv, self.etu])
+        self.add([self.tv, self.ti, self.tmax, self.tmin])
+        self.add([self.ln, self.td])
+        self.add([self.hi, self.hv, self.ri, self.rv])
+        self.add([self.pi, self.pv, self.wi])
 
         self.clear_all()
 
     def clear_all(self):
         pass
-#        self.ewv.set(0.0)
-#        self.egv.set(0.0)
-#        self.eev.set(0.0)
-#        self.etv.set(0.0)
-
 
     def set_date_time(self):
-#        tdate = time.strftime('%d-%b-%y')
         self.cdate.set(time.strftime('%d-%b'))
         self.ctime.set(time.strftime('%H:%M'))
         
-    def update_total(self):
-        pass
-        #self.etv.set(self.eev.value)
 
 if __name__ == '__main__':
 
     from lcd import LCD
 
     # Display Layout instance
-    L2 = Layout_EUR()
+    L2 = LWeather()
 
     # LCD instance
     lcd = LCD(False)       
